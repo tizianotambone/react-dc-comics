5b2b3b96c84dc2285557cc9ae350af8975f9fd9c
tizianotambone <tambonetiziano@gmail.com>
2025-05-05 15:28:00 +0200

    first commit

---
+++ react-dc-comics/src/components/header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import img from '../assets/img/dc-logo.png'
+
+const header = () => {
+  return (
+    
+    <header class="dc-header">
+    <div class="logo">
+        <img src={img} alt="DC Comics Logo" />
+    </div>
+    <nav class="main-nav">
+      <ul>
+        <li><a href="#">Characters</a></li>
+        <li><a href="#" class="active">Comics</a></li>
+        <li><a href="#">Movies</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">Games</a></li>
+        <li><a href="#">Collectibles</a></li>
+        <li><a href="#">Videos</a></li>
+        <li><a href="#">Fans</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Shop</a></li>
+      </ul>
+    </nav>
+  </header>
+  )
+}
+
+export default header
