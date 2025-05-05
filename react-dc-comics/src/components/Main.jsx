2bde39b0367ff2cb63ba6637fc3d09c270e0ff2c
tizianotambone <tambonetiziano@gmail.com>
2025-05-05 15:54:42 +0200

    [UPD] main done

--- react-dc-comics/src/components/Main.jsx
+++ react-dc-comics/src/components/Main.jsx
@@ -1,39 +1,44 @@
 import React from 'react'
+import img1 from '../assets/img/buy-comics-digital-comics.png'
+import img2 from '../assets/img/buy-comics-merchandise.png'
+import img3 from '../assets/img/buy-comics-shop-locator.png'
+import img4 from '../assets/img/buy-comics-subscriptions.png'
+import img5 from '../assets/img/buy-dc-power-visa.svg'
 
 
+const Main = () => {
+    return (
+        <main>
 
-const  Main = () => {
-  return (
-    <main>
-  
-  <section className="black-main">
-    <h2>--&gt; Content goes here &lt;--</h2>
-  </section>
+            <section className="black-main">
+                <h2>--&gt; Content goes here &lt;--</h2>
+            </section>
+            
+            <section className="blue-main">
+                <div class="img1">
+                    <img src={img1} alt="" />
+                    <span>Digital Comics</span>
+                </div>
+                <div className="img1">
+                    <img src={img2} alt="" />
+                    <span>DC Merchandise</span>
+                </div>
+                <div className="img1">
+                    <img src={img3} alt="" />
+                    <span>Subscription</span>
+                </div>
+                <div className="img1">
+                    <img src={img4} alt="" />
+                    <span>Comic Shop Locator</span>
+                </div>
+                <div className="img1">
+                    <img src={img5} alt="" />
+                    <span>DC Power Visa</span>
+                </div>
+            </section>
+        </main>
 
-  
-  <section className="blue-main">
-    <div class="img1">
-     
-      <span>Digital Comics</span>
-    </div>
-    <div className="img1">
-      
-      <span>DC Merchandise</span>
-    </div>
-    <div className="img1">
-      
-      <span>Subscription</span>
-    </div>
-    <div className="img1">
-      <span>Comic Shop Locator</span>
-    </div>
-    <div className="img1">
-      <span>DC Power Visa</span>
-    </div>
-  </section>
-</main>
-
-  )
+    )
 
 }
 
