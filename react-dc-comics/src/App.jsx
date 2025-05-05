6822e94fa4c3ad3de2ea3e71cc17c6de9aefd346
tizianotambone <tambonetiziano@gmail.com>
2025-05-05 18:08:45 +0200

    [UPD]fix ex

--- react-dc-comics/src/App.jsx
+++ react-dc-comics/src/App.jsx
@@ -1,7 +1,7 @@
 
 import { Fragment } from 'react'
 import './App.css'
-import Header from './components/header'  
+import Header from './components/Header'  
 import Main from './components/Main'
 import Footer from './components/Footer'
 
